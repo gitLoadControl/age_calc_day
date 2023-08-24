668ee73853c11f508ae6a80c76991e66fddfc21f
masyanov <masyanov.aleksei@yandex.ru>
2023-08-24 10:19:31 +0300

    Дописал функцию склонения подписи к дням. Поправил стили кнопки

--- main.js
+++ main.js
@@ -15,10 +15,16 @@ function  calculationDays(event) {
   // Рассчитываем количество дней
   const numberDays = (Date.now() - Date.parse(date.value)) / 86400000;
 
+  // Подготавливаем подпись
+  function declOfNum(number, words) {  
+    return words[(number % 100 > 4 && number % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(number % 10 < 5) ? Math.abs(number) % 10 : 5]];
+  }
+
   // Выводим количество дней с округление до целого
   days.innerHTML= Math.floor(numberDays);
+
+  // Выводим подпись
+  days.innerHTML= "Ты живешь: " + Math.floor(numberDays) + declOfNum(Math.floor(numberDays), [' день', ' дня', ' дней']);
 }
 
 button.addEventListener('click', calculationDays);
-
-
