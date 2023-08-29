462dc804a25ac6b517a62052b00883fdd98fd948
Dmitriy Ryzhkov <nimens55@gmail.com>
2023-08-29 21:04:40 +0300

    Рефакторинг кода

--- main.js
+++ main.js
@@ -1,30 +1,39 @@
+/************* Helpers *************/
+// Проверяет данные на заполнение
+function isValidate(data) {
+  return data === "" ? true : false;
+}
+
+// Возвращает нужное склонение
+function declOfNum(number, words) {
+  return words[(number % 100 > 4 && number % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(number % 10 < 5) ? Math.abs(number) % 10 : 5]];
+}
+
+/************* Application *************/
 // Расчет дней от указанной доты
 const button = document.getElementById('submit-button');
 const date = document.getElementById('date-born');
 const days = document.getElementById('days');
 
+// Рассчитывает количество дней
+function  getDays(date) {
+  return Math.floor((Date.now() - Date.parse(date)) / 86400000);
+}
+
 function  calculationDays(event) {
   event.preventDefault();
 
   // Проверяем заполнено ли значение в input
-  if(date.value === "") {
+  if(isValidate(date.value)) {
     console.log('Поле не заполнено');
     return;
   }
 
   // Рассчитываем количество дней
-  const numberDays = (Date.now() - Date.parse(date.value)) / 86400000;
+  const numberDays = getDays(date.value);
 
-  // Подготавливаем подпись
-  function declOfNum(number, words) {  
-    return words[(number % 100 > 4 && number % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(number % 10 < 5) ? Math.abs(number) % 10 : 5]];
-  }
-
-  // Выводим количество дней с округление до целого
-  days.innerHTML= Math.floor(numberDays);
-
-  // Выводим подпись
-  days.innerHTML= Math.floor(numberDays) + declOfNum(Math.floor(numberDays), [' день', ' дня', ' дней']);
+  // Выводим результаты пользователю
+  days.innerHTML= numberDays + declOfNum(numberDays, [' день', ' дня', ' дней']);
 }
 
 button.addEventListener('click', calculationDays);
